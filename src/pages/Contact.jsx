0607f2b7972b56974bed0b97a42908774e373e07
izafi <huzaifazafi156@gmail.com>
2026-09-05 16:10:27 +0500

    feat: implement Contact page layout with components for user engagement

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import ContactHero from "../components/contact/ContactHero";
+import ContactInfo from "../components/contact/ContactInfo";
+import ContactForm from "../components/contact/ContactForm";
+import ContactCTA from "../components/contact/ContactCTA";
 
 const Contact = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <main>
+      <ContactHero />
+      <ContactInfo />
+      <ContactForm />
+      <ContactCTA />
+    </main>
+  );
+};
 
-export default Contact
+export default Contact;
