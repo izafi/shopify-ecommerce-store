f2b22c04b0ea46dbd8497f7d6a2edb8e983355ff
izafi <huzaifazafi156@gmail.com>
2026-09-05 16:06:14 +0500

    feat: restructure About component to include all sections for enhanced storytelling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import AboutHero from "../components/about/AboutHero";
+import AboutStory from "../components/about/AboutStory";
+import AboutStats from "../components/about/AboutStats";
+import AboutFeatures from "../components/about/AboutFeatures";
+import AboutCTA from "../components/about/AboutCTA";
 
 const About = () => {
   return (
-    <div>
-    
-    </div>
-  )
-}
+    <main>
+      <AboutHero />
+      <AboutStory />
+      <AboutStats />
+      <AboutFeatures />
+      <AboutCTA />
+    </main>
+  );
+};
 
-export default About
+export default About;
